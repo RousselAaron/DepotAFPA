09a3e98dc8c09273c7df22a0fb896e5eef2b0f89
80010-84-10 <80010-84-10@exchange.ad.afpanet>
2020-02-20 16:43:50 +0100

    début exercice regExp sur un form 50%

--- js DOM/regEx formulaire/regexp.js
+++ js DOM/regEx formulaire/regexp.js
@@ -1,30 +1,59 @@
-var regexnom = new RegExp("^[a-zA-Z]+$");
-var chknm;
-// element = document.getElementById("input1").value;
-// console.log(element)
-// if (regexnom.test("element")) {
-//     console.log("ok");
-// }
-var element = document.getElementById("button1");
-element.addEventListener("click", () => {
-    chknm = document.getElementById("input2").value;
-    console.log(chknm)
-    if (regexnom.test(chknm) == true) {
-        console.log("ok");
-        return chknm;
-    } else {
-        event.preventDefault()
-        document.getElementById("button1").removeAttribute("type");
-        console.log("button1");
-    }
-    chkgender = document.getElementById("input4").value;
-    console.log(chkgender)
-    if (regexnom.test(chkgender) == true) {
-        console.log("ok");
-        return chkgender;
-    } else {
-        event.preventDefault()
-        document.getElementById("button1").removeAttribute("type");
-        console.log("button1");
+var x;
+var a;
+var filtre;
+var tabRegExp = [new RegExp("^[a-zA-Z]+$"), new RegExp("^[a-zA-Z0-9]+$")];
+var tabErrorMess = ["", ""]
+console.log(tabRegExp[0]);
+console.log(tabRegExp[1]);
+
+form.addEventListener("submit", (element) => {
+    console.log("azeazeazeazeaze")
+    element.preventDefault();
+    checkForm(form);
+
+    function checkForm(form) {
+        form = document.getElementById('form');
+        filtre = tabRegExp[0];
+        var a = filtre.test(form.elements['family-name'].value);
+        console.log(a);
+
+        filtre = tabRegExp[0];
+        var b = filtre.test(form.elements['given-name'].value);
+        console.log(b);
+
+        filtre = new RegExp("^[0-9][0-9]?\/[0-9][0-9]?\/[0-9][0-9][0-9][0-9]?$");
+        var c = filtre.test(form.elements['bday'].value);
+        console.log(c);
+
+        filtre = new RegExp("^[0-9]+$");
+        var d = filtre.test(form.elements['cp'].value);
+        console.log(d);
+
+        filtre = tabRegExp[1];
+        var e = filtre.test(form.elements['adresse'].value);
+        console.log(e);
+
+        filtre = tabRegExp[0];
+        var f = filtre.test(form.elements['ville'].value);
+        console.log(f);
+
+        filtre = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[az0-9])?");
+        var g = filtre.test(form.elements['courriel'].value);
+        console.log(g);
+
+        filtre = tabRegExp[1];
+        var h = filtre.test(form.elements['questions'].value);
+        console.log(h);
+
+        filtre = tabRegExp[1];
+        var i = filtre.test(form.elements['textquestions'].value);
+        console.log(i);
+
+        if (a && b && c && d && e && f && g && h && i) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 });
