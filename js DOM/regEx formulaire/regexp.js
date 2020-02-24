5c707bfa8a93b879f53c80d529c3220348ab8fbb
unknown <roussel.aaron.tr@gmail.com>
2020-02-24 18:52:20 +0100

    formulaire 90%

--- js DOM/regEx formulaire/regexp.js
+++ js DOM/regEx formulaire/regexp.js
@@ -1,197 +1,83 @@
-var x;
-var a;
-var y;
-var w;
-var filtre;
 var tabRegExp = [new RegExp("^[a-zA-Z0-9]$"), new RegExp("^[a-zA-Z]+$"), new RegExp("^[0-9][0-9]?\/[0-9][0-9]?\/[0-9][0-9][0-9][0-9]?$"), new RegExp("^[0-9]+$"), new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[az0-9])?")];
-// tab RegEx: 0-only string, 1-string & number, 2-Bday only, 3-number only, 4-Email.
+// tab RegEx: 0-string & number, 1-string only, 2-Bday only, 3-number only, 4-Email.
 var tabErrorMess = ["ce champs doit etre renseigné", "<p>ce champs doit etre renseigné et doit contenir uniquement des lettres</p>", "<p>ce champs doit etre renseigné et doit contenir une date de naissance valide <br>exemple jj/mm/aaaa</p>", "<p>ce champs doit etre renseigné et doit contenir uniquement des nombres</p>", "<p>ce champs doit etre renseigné et doit contenir une adresse mail valide <br>exemple :jhon@wick.com</p>"];
-var tabError = ['errorLname', 'errorGname', 'errorBday', 'errorCdp', 'errorAdrss', 'errorVille', 'errorEmail', 'errorTexta']
+var tabErrorId = ['errorLname', 'errorGname', 'genre', 'errorBday', 'errorCdp', 'errorAdrss', 'errorVille', 'errorEmail', 'errorTexta'];
+var tabInputName = ['familyName', 'givenName', 'errorGenre', 'bday', 'cp', 'adresse', 'ville', 'courriel', 'questions', 'textquestions'];
+var tabIsValid = Array(11);
 
-// form.addEventListener("submit", (element) => {
-//     element.preventDefault();
-//     checkForm(form);
+// nom
+champ(0, 1);
+// prénom
+champ(1, 1);
+// genre
+genre(2);
+//Bday
+champ(3, 2);
+//code postale 
+champ(4, 3);
+//adresse
+champ(5, 0);
+//ville
+champ(6, 1);
+//adresse email
+champ(7, 4);
+//sujet de la question
+champ(8, 5);
+//zone de texte pour la question
+champ(9, 0);
+//validations du formulaire 
+ValidForm(10);
 
-//     function checkForm(form) {
-//         form = document.getElementById('form');
+function champ(x, w) {
+    let ele = document.getElementById(tabInputName[x]);
+    ele.addEventListener("change", (element) => {
+        element.preventDefault();
+        verifChmp(x, w, ele);
+        ErrorMessage(error, x, tabErrorId, w);
+        return ele;
+    });
+}
 
-//         w = 1;
-//         filtre = tabRegExp[w];
-//         var a = filtre.test(form.elements['family-name'].value);
-//         console.log(a);
-//         error = a;
-//         y = 0;
-//         ErrorMessage(error, y, tabError);
-
-//         w = 1;
-//         filtre = tabRegExp[w];
-//         var b = filtre.test(form.elements['given-name'].value);
-//         console.log(b);
-//         error = b;
-//         y = 1;
-//         ErrorMessage(error, y, tabError);
-
-//         w = 2;
-//         filtre = tabRegExp[w];
-//         var c = filtre.test(form.elements['bday'].value);
-//         console.log(c);
-//         error = c;
-//         y = 2;
-//         ErrorMessage(error, y, tabError);
-
-//         w = 3;
-//         filtre = tabRegExp[w];
-//         var d = filtre.test(form.elements['cp'].value);
-//         console.log(d);
-//         error = d;
-//         y = 3;
-//         ErrorMessage(error, y, tabError);
-
-//         w = 0;
-//         filtre = tabRegExp[w];
-//         console.log(tabRegExp[w])
-//         var e = filtre.test(form.elements['adresse'].value);
-//         console.log(e);
-//         error = e;
-//         y = 4;
-//         ErrorMessage(error, y, tabError);
-
-//         w = 1;
-//         filtre = tabRegExp[w];
-//         var f = filtre.test(form.elements['ville'].value);
-//         console.log(f);
-//         error = f;
-//         y = 5;
-//         ErrorMessage(error, y, tabError);
-
-//         w = 4;
-//         filtre = tabRegExp[w];
-//         var g = filtre.test(form.elements['courriel'].value);
-//         console.log(g);
-//         error = g;
-//         y = 6;
-//         ErrorMessage(error, y, tabError);
-
-//         w = 0;
-//         filtre = tabRegExp[w];
-//         var h = filtre.test(form.elements['questions'].value);
-//         console.log(h);
-//         error = h;
-//         y = 7;
-//         ErrorMessage(error, y, tabError);
-
-//         w = 0;
-//         filtre = tabRegExp[w];
-//         var i = filtre.test(form.elements['textquestions'].value);
-//         console.log(i);
-//         error = i;
-//         y = 8;
-//         ErrorMessage(error, y, tabError);
-
-//         if (a && b && c && d && e && f && g && h && i) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-
-//     }
-// });
-
-function ErrorMessage(error, y, tabError) {
-    if (error == false) {
-        document.getElementById(tabError[y]).innerHTML = tabErrorMess[w];
-        console.log(tabError[y]);
-        console.log(tabErrorMess[w]);
+function verifChmp(x, w, ele) {
+    if (ele.value != undefined) {
+        let filtre = tabRegExp[w];
+        tabIsValid[x] = filtre.test(ele.value);
+        error = tabIsValid[x];
+        return error;
     } else {
-        document.getElementById(tabError[y]).innerHTML = "";
+        error = tabIsValid[x];
     }
 }
-form.addEventListener("keyup", (element) => {
 
+function ErrorMessage(error, x, tabErrorId, w) {
+    if (error == false) {
+        document.getElementById(tabErrorId[x]).innerHTML = tabErrorMess[w];
+    } else {
+        document.getElementById(tabErrorId[x]).innerHTML = "";
+    }
+}
+
+form.addEventListener("submit", (element) => {
     element.preventDefault();
-    checkForm(form);
-
-    function checkForm(form) {
-        form = document.getElementById('form');
-
-        w = 1;
-        filtre = tabRegExp[w];
-        var a = filtre.test(form.elements['family-name'].value);
-        console.log(a);
-        error = a;
-        y = 0;
-        ErrorMessage(error, y, tabError);
-
-        w = 1;
-        filtre = tabRegExp[w];
-        var b = filtre.test(form.elements['given-name'].value);
-        console.log(b);
-        error = b;
-        y = 1;
-        ErrorMessage(error, y, tabError);
-
-        w = 2;
-        filtre = tabRegExp[w];
-        var c = filtre.test(form.elements['bday'].value);
-        console.log(c);
-        error = c;
-        y = 2;
-        ErrorMessage(error, y, tabError);
-
-        w = 3;
-        filtre = tabRegExp[w];
-        var d = filtre.test(form.elements['cp'].value);
-        console.log(d);
-        error = d;
-        y = 3;
-        ErrorMessage(error, y, tabError);
-
-        w = 0;
-        filtre = tabRegExp[w];
-        console.log(tabRegExp[w])
-        var e = filtre.test(form.elements['adresse'].value);
-        console.log(e);
-        error = e;
-        y = 4;
-        ErrorMessage(error, y, tabError);
-
-        w = 1;
-        filtre = tabRegExp[w];
-        var f = filtre.test(form.elements['ville'].value);
-        console.log(f);
-        error = f;
-        y = 5;
-        ErrorMessage(error, y, tabError);
-
-        w = 4;
-        filtre = tabRegExp[w];
-        var g = filtre.test(form.elements['courriel'].value);
-        console.log(g);
-        error = g;
-        y = 6;
-        ErrorMessage(error, y, tabError);
-
-        w = 0;
-        filtre = tabRegExp[w];
-        var h = filtre.test(form.elements['questions'].value);
-        console.log(h);
-        error = h;
-        y = 7;
-        ErrorMessage(error, y, tabError);
-
-        w = 0;
-        filtre = tabRegExp[w];
-        var i = filtre.test(form.elements['textquestions'].value);
-        console.log(i);
-        error = i;
-        y = 8;
-        ErrorMessage(error, y, tabError);
-
-        if (a && b && c && d && e && f && g && h && i) {
-            return true;
-        } else {
-            return false;
-        }
-
+    genre(2);
+    if (tabIsValid.includes(false)) {
+        return false;
+    } else {
+        return true;
     }
 });
+
+function genre(x) {
+    let ele = document.getElementById(tabInputName[x]);
+    console.log(ele.value);
+    ele.addEventListener("change", (element) => {
+        console.log(ele);
+        if (ele.value == ("Masculin" || "Feminin")) {
+            console.log('aaaa');
+            tabIsValid[x] = true;
+        } else {
+            tabIsValid = false;
+            document.getElementById(tabErrorId[x]).innerHTML = "Veuillez indiquer votre genre"
+        }
+    });
+}
